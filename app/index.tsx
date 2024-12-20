9d12059b8edaf727bd2406f8c92fe70f01ed997f
YoussefSayedDev <youssef.el.sayed.email@gmail.com>
2024-12-20 02:01:30 +0200

    first commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,14 @@
-import { Stack, Link } from 'expo-router';
+import { Link, Stack } from 'expo-router';
 
+import React from 'react';
+import { Text, View } from 'react-native';
 import { Button } from '~/components/Button';
 import { Container } from '~/components/Container';
-import { ScreenContent } from '~/components/ScreenContent';
 
 export default function Home() {
   return (
-    <>
-      <Stack.Screen options={{ title: 'Home' }} />
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Home" />
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          <Button title="Show Details" />
-        </Link>
-      </Container>
-    </>
+    <View>
+      <Text className="mt-5 text-center text-3xl text-red-500">Home</Text>
+    </View>
   );
 }
