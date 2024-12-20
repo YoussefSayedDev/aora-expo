55d1d3e997d3b523765dcd50894304386298f102
YoussefSayedDev <youssef.el.sayed.email@gmail.com>
2024-12-20 04:26:26 +0200

    Finish the index screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,45 @@
-import { Link, Stack } from 'expo-router';
+import { Redirect, router } from 'expo-router';
+import { Image, ScrollView, StatusBar, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import CustomButton from '~/components/CustomButton';
+import { images } from '../constants';
 
-import React from 'react';
-import { Text, View } from 'react-native';
-import { Button } from '~/components/Button';
-import { Container } from '~/components/Container';
-
-export default function Home() {
+export default function App() {
   return (
-    <View>
-      <Text className="mt-5 text-center text-3xl text-red-500">Home</Text>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView contentContainerStyle={{height: '100%'}} >
+        <View className='w-full justify-center items-center h-full px-4'>
+          <Image 
+            source={images.logo}
+            className='w-[130px] h-[84px]' 
+            resizeMode='contain'
+          />
+          <Image 
+            source={images.cards}
+            className='max-w-[380px] w-full h-[300px]'
+            resizeMode='contain'
+          />
+          <View className='relative mt-5'>
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless Possibilities with <Text className='text-secondary-200'>Aora</Text>
+            </Text>
+            <Image 
+              source={images.path}
+              className='w-[136px] h-[15px] absolute -bottom-2 -right-8'
+              resizeMode='contain'
+            />
+          </View>
+          <Text className='text-sm font-pregular text-gray-100 mt-7 text-center'>
+            Where creativity meets innovation: embark on a journey of discovery
+          </Text>
+          <CustomButton
+          title="Continue with Email"
+          handlePress={() => {router.push('/sign-in')}}
+          containerStyle="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor="#161622" barStyle={'light-content'} />
+    </SafeAreaView>
   );
 }
